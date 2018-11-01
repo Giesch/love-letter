65ea391a0b057c1503f01301e1fb09290a0ff192
Dan Knutson <dan.knutson@gmail.com>
2018-11-01 02:20:52 -0500

    actix serves the react app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate actix_web;
+
+use actix_web::{fs, server, App};
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| {
+        App::new().handler(
+            "/",
+            fs::StaticFiles::new("./love-letter-ui/dist")
+                .unwrap()
+                .show_files_listing()
+                .index_file("index.html"),
+        )
+    }).bind("127.0.0.1:8888")
+    .unwrap()
+    .run();
 }
