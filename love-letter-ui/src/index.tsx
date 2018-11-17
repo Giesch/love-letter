f63d1a0237659c5a71f1aab3f8030cc5597fdcf9
Dan Knutson <dan.knutson@gmail.com>
2018-11-16 18:01:03 -0600

    actually remove redux devtools

--- love-letter-ui/src/index.tsx
+++ love-letter-ui/src/index.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Store, createStore, applyMiddleware, Reducer, AnyAction } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 import { Root } from './root';
 import { ApplicationState } from './store';
 import { rootReducer } from './store/index';
 import { configureStore } from './configureStore';
-
-const composeEnhancers = composeWithDevTools({})
 
 const initialState: any = undefined;
 
