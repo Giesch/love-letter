3ebf68d5ec9caead2c5c0fab2295e13e0a486927
Dan Knutson <dan.knutson@gmail.com>
2018-11-20 21:07:34 -0600

    add delete room endpoint

--- src/app/rooms.rs
+++ src/app/rooms.rs
@@ -30,6 +30,7 @@ pub struct InitialRoom {
 pub trait RoomsApi {
     fn open_rooms(&self) -> Vec<Room>;
     fn create_room(&self, room: InitialRoom) -> Room;
+    fn delete_room(&self, room_id: &str) -> bool;
 }
 
 impl RoomsApi for AppState {
@@ -41,10 +42,14 @@ impl RoomsApi for AppState {
     fn create_room(&self, room: InitialRoom) -> Room {
         let room = Room::new(room);
         let mut rooms = self.open_rooms.lock().unwrap();
-        // much clone wow
         (*rooms).insert(room.id.clone(), room.clone());
 
         room
+    }
+
+    fn delete_room(&self, room_id: &str) -> bool {
+        let mut rooms = self.open_rooms.lock().unwrap();
+        (*rooms).remove(room_id).is_some()
     }
 }
 
@@ -54,7 +59,7 @@ mod tests {
 
     #[test]
     fn create_and_read_room() {
-        let mut state = AppState::new();
+        let state = AppState::new();
         assert_eq!(Vec::<Room>::new(), state.open_rooms());
 
         let request = InitialRoom {
@@ -64,10 +69,35 @@ mod tests {
 
         state.create_room(request);
 
-        let result: Vec<Room> = state.open_rooms();
+        let result = state.open_rooms();
         assert_eq!(1, result.len());
         let room = result.get(0).unwrap();
         assert_eq!("example", room.name);
         assert_eq!(Some(&"somebody".to_string()), room.players.iter().next());
+    }
+
+    #[test]
+    fn create_and_delete_room() {
+        let state = AppState::new();
+        let request = InitialRoom {
+            name: "example".to_string(),
+            player: "somebody".to_string(),
+        };
+
+        state.create_room(request);
+
+        let result = state.open_rooms();
+        assert_eq!(1, result.len());
+
+        let room = result.get(0).unwrap();
+        assert_eq!("example", room.name);
+
+        let target_id = &room.id;
+
+        let deleted = state.delete_room(target_id);
+        assert!(deleted);
+
+        let result = state.open_rooms();
+        assert!(result.is_empty())
     }
 }
